6ca67a5f270ac34df4b3244712c1c04a570187f2
a1824jj <a1824jj@aiit.ac.jp>
2020-04-03 12:49:03 +0900

    convert mvc -> mvvm

---
+++ app/src/main/java/a1824jj/jp/ac/aiit/modernarchitectures_sampel/mvvm/CountriesViewModel.java
@@ -0,0 +1,61 @@
+package a1824jj.jp.ac.aiit.modernarchitectures_sampel.mvvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import a1824jj.jp.ac.aiit.modernarchitectures_sampel.model.CountriesService;
+import a1824jj.jp.ac.aiit.modernarchitectures_sampel.model.Country;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class CountriesViewModel extends ViewModel {
+
+    private final MutableLiveData<List<String>> countries = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> countryError = new MutableLiveData<>();
+
+    private CountriesService service;
+
+    public CountriesViewModel(){
+        service = new CountriesService();
+        fetchCountries();
+    }
+
+    public LiveData<List<String>> getCountries() {
+        return countries;
+    }
+
+    public LiveData<Boolean> getCountryError() {
+        return countryError;
+    }
+
+    private void fetchCountries(){
+        service.getCountries()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableSingleObserver<List<Country>>() {
+
+                    @Override
+                    public void onSuccess(List<Country> value) {
+                        List<String> countryNames = new ArrayList<>();
+                        for(Country country: value){
+                            countryNames.add(country.countryName);
+                        }
+                        countries.setValue(countryNames);
+                        countryError.setValue(false);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        countryError.setValue(true);
+                    }
+                });
+    }
+
+    public void onRefresh(){
+        fetchCountries();
+    }
+}
--- app/src/main/java/a1824jj/jp/ac/aiit/modernarchitectures_sampel/mvvm/MVVMActivity.java
+++ app/src/main/java/a1824jj/jp/ac/aiit/modernarchitectures_sampel/mvvm/MVVMActivity.java
@@ -2,10 +2,21 @@ package a1824jj.jp.ac.aiit.modernarchitectures_sampel.mvvm;
 
 import a1824jj.jp.ac.aiit.modernarchitectures_sampel.R;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MVVMActivity extends AppCompatActivity {
 
@@ -14,9 +25,62 @@ public class MVVMActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mvvm);
         setTitle("MVVM Activity");
+
+        viewModel = new ViewModelProvider(this).get(CountriesViewModel.class);
+
+        list = findViewById(R.id.list);
+        retryButton = findViewById(R.id.retryButton);
+        progress = findViewById(R.id.progress);
+        adapter = new ArrayAdapter<>(this, R.layout.row_layout, R.id.listText, listValues);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MVVMActivity.this, "You Clicked " + listValues.get(position), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        observeViewModel();
     }
 
     public static Intent getIntent(Context context){
         return new Intent(context, MVVMActivity.class);
+    }
+
+    private List<String> listValues = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private ListView list;
+    private CountriesViewModel viewModel;
+    private Button retryButton;
+    private ProgressBar progress;
+
+    public void onRetry(View view){
+        viewModel.onRefresh();
+        retryButton.setVisibility(View.GONE);
+        progress.setVisibility(View.VISIBLE);
+        list.setVisibility(View.GONE);
+    }
+
+    private void observeViewModel() {
+        viewModel.getCountries().observe(this, countries -> {
+            if(countries != null){
+                listValues.clear();
+                listValues.addAll(countries);
+                list.setVisibility(View.VISIBLE);
+                adapter.notifyDataSetChanged();
+            }else{
+                list.setVisibility(View.GONE);
+            }
+        });
+
+        viewModel.getCountryError().observe(this, error ->{
+            progress.setVisibility(View.GONE);
+            if(error){
+                Toast.makeText(MVVMActivity.this, "Unable to get country names. Please retry later", Toast.LENGTH_LONG).show();
+                retryButton.setVisibility(View.VISIBLE);
+            }else{
+                retryButton.setVisibility(View.GONE);
+            }
+        });
     }
 }
